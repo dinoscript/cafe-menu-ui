e1946b345d28ec584216752773ba06967b278f25
VS <skoroplias.v@gmail.com>
2017-09-06 14:08:34 +0300

    add validation

--- src/app/dish-add/dish-add.component.js
+++ src/app/dish-add/dish-add.component.js
@@ -11,29 +11,26 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 Object.defineProperty(exports, "__esModule", { value: true });
 require("rxjs/add/operator/switchMap");
 var core_1 = require("@angular/core");
-var router_1 = require("@angular/router");
 var common_1 = require("@angular/common");
 var forms_1 = require("@angular/forms");
-var dish_1 = require("../classes/dish");
 var dish_service_1 = require("../services/dish.service");
 var DishAddComponent = (function () {
-    function DishAddComponent(dishService, route, location, fb) {
+    function DishAddComponent(dishService, location, fb) {
         this.dishService = dishService;
-        this.route = route;
         this.location = location;
         this.fb = fb;
         this.createForm();
     }
     DishAddComponent.prototype.ngOnInit = function () {
-        this.dish = new dish_1.Dish;
     };
-    DishAddComponent.prototype.add = function (dish) {
+    DishAddComponent.prototype.add = function (params) {
         var _this = this;
-        dish.params.name = dish.params.name.trim();
-        if (!dish.params.name) {
+        params.name = params.name.trim();
+        if (!params.name) {
+            console.log(this.addForm);
             return;
         }
-        this.dishService.create(dish.params)
+        this.dishService.create(params)
             .then(function (dish) {
             console.log(dish);
             _this.goBack();
@@ -44,22 +41,36 @@ var DishAddComponent = (function () {
     };
     DishAddComponent.prototype.createForm = function () {
         this.addForm = this.fb.group({
-            name: this.dishService.selectedDish ? this.dishService.selectedDish.params.name : '',
-            price: this.dishService.selectedDish ? this.dishService.selectedDish.params.price : 0,
+            name: [this.dishService.selectedDish ? this.dishService.selectedDish.params.name : '',
+                [forms_1.Validators.required, forms_1.Validators.minLength(4), this.nameValidator, this.forbiddenNameValidator(/bob/i)]
+            ],
+            price: this.dishService.selectedDish ? this.dishService.selectedDish.params.price : '',
             imgUrl: this.dishService.selectedDish ? this.dishService.selectedDish.params.imgUrl : '',
             dateFrom: this.dishService.selectedDish ? this.dishService.selectedDish.params.dateFrom : '',
             dateTo: this.dishService.selectedDish ? this.dishService.selectedDish.params.dateTo : ''
         });
+    };
+    DishAddComponent.prototype.nameValidator = function (control) {
+        if (control.value === "nonono") {
+            return { "userName": true };
+        }
+        return null;
+    };
+    DishAddComponent.prototype.forbiddenNameValidator = function (nameRe) {
+        return function (control) {
+            var forbidden = nameRe.test(control.value);
+            return forbidden ? { 'forbiddenName': { value: control.value } } : null;
+        };
     };
     return DishAddComponent;
 }());
 DishAddComponent = __decorate([
     core_1.Component({
         selector: 'dish-add',
-        templateUrl: './dish-add.component.html'
+        templateUrl: './dish-add.component.html',
+        styleUrls: ['./dish-add.component.css']
     }),
     __metadata("design:paramtypes", [dish_service_1.DishService,
-        router_1.ActivatedRoute,
         common_1.Location,
         forms_1.FormBuilder])
 ], DishAddComponent);
--- src/app/dish-add/dish-add.component.ts
+++ src/app/dish-add/dish-add.component.ts
@@ -1,37 +1,34 @@
 import 'rxjs/add/operator/switchMap';
 import { Component, OnInit }        from '@angular/core';
-import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Location }                 from '@angular/common';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
-import { Dish }        from '../classes/dish';
 import { DishService } from '../services/dish.service';
 
 @Component({
     selector: 'dish-add',
-    templateUrl: './dish-add.component.html'
+    templateUrl: './dish-add.component.html',
+    styleUrls: ['./dish-add.component.css']
 })
 export class DishAddComponent implements OnInit {
-    dish:Dish;
-    addForm: FormGroup;
+    addForm:FormGroup;
 
     constructor(private dishService:DishService,
-                private route:ActivatedRoute,
                 private location:Location,
-                private fb: FormBuilder) {
+                private fb:FormBuilder) {
         this.createForm();
     }
 
     ngOnInit():void {
-        this.dish = new Dish;
     }
 
-    add(dish:Dish):void {
-        dish.params.name = dish.params.name.trim();
-        if (!dish.params.name) {
+    add(params:any):void {
+        params.name = params.name.trim();
+        if (!params.name) {
+            console.log(this.addForm);
             return;
         }
-        this.dishService.create(dish.params)
+        this.dishService.create(params)
             .then(dish => {
                 console.log(dish);
                 this.goBack()
@@ -44,11 +41,28 @@ export class DishAddComponent implements OnInit {
 
     createForm() {
         this.addForm = this.fb.group({
-          name: this.dishService.selectedDish?this.dishService.selectedDish.params.name:'',
-          price: this.dishService.selectedDish?this.dishService.selectedDish.params.price:0,
-          imgUrl: this.dishService.selectedDish?this.dishService.selectedDish.params.imgUrl:'',
-          dateFrom: this.dishService.selectedDish?this.dishService.selectedDish.params.dateFrom:'',
-          dateTo: this.dishService.selectedDish?this.dishService.selectedDish.params.dateTo:''
+            name: [this.dishService.selectedDish ? this.dishService.selectedDish.params.name : '',
+                [Validators.required, Validators.minLength(4), this.nameValidator, this.forbiddenNameValidator(/bob/i)]
+            ],
+            price: this.dishService.selectedDish ? this.dishService.selectedDish.params.price : '',
+            imgUrl: this.dishService.selectedDish ? this.dishService.selectedDish.params.imgUrl : '',
+            dateFrom: this.dishService.selectedDish ? this.dishService.selectedDish.params.dateFrom : '',
+            dateTo: this.dishService.selectedDish ? this.dishService.selectedDish.params.dateTo : ''
         });
+    }
+
+    nameValidator(control:FormControl):{[s:string]:boolean} {
+
+        if (control.value === "nonono") {
+            return {"userName": true};
+        }
+        return null;
+    }
+
+    forbiddenNameValidator(nameRe:RegExp):ValidatorFn {
+        return (control:AbstractControl):{[key: string]: any} => {
+            const forbidden = nameRe.test(control.value);
+            return forbidden ? {'forbiddenName': {value: control.value}} : null;
+        };
     }
 }
